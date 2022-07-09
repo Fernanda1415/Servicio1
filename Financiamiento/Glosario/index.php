7b51d911503c6101535c35e4ff47b219ffb32ad2
Fernanda1415 <fernanda.martinez.t010@gmail.com>
2022-07-08 22:44:09 -0500

    carpetas

---
+++ Financiamiento/Glosario/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+
+    <!-- Normalize CSS -->
+	  <link rel="stylesheet" href="./css/normalize.css">    
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">  
+    <!-- Iconos -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <!-- Malihu jQuery custom content scroller CSS -->
+	  <link rel="stylesheet" href="./css/jquery.mCustomScrollbar.css">    
+    <!-- Confirm -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+    <!-- MaterialDark CSS -->
+	  <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/validetta.css">
+
+    <script src="https://kit.fontawesome.com/49f2039b91.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+       
+
+</head>
+<body class="stretched sticky-responsive-menu" data-loader="7">
+    <header>
+        
+    <div class="navbar-fixed">
+    <nav class="cyan darken-4">
+      <div class="row">
+        <div class="col l2 m2 s2 center">
+            <img src="./assets/img/IPNBlancoyNegro.png" class="rensponsive-img"  alt="LogoIPN" width="43%">
+        </div>
+        <div class="col l8 m8 s8">
+          <div class="nav-wrapper">
+            <a href="http://localhost/servicio/" class="brand-logo"><i class="fa-solid fa-house-chimney fa-2x"></i></a>
+            <ul class="right hide-on-med-and-down">
+            <li><a href="http://localhost/TT2/Conocenos.php">Conócenos</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col l2 m2 s2 center">
+          <img src="./assets/img/escudoESCOM1.png" class="rensponsive-img"  alt="LogoIPN" width="37%">
+        </div>
+      </div>
+    </nav> 
+    </div>
+    
+
+    </header>
+   
+  <div class="row">
+
+  </div>  
+  <div class="row ">
+    <?php include("index.html");?>
+  </div>
+      
+
+
+       
+
+
+  <!--Footer-->
+    <footer class="page-footer grey darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Polilibro</h5>
+                <p class="grey-text text-lighten-4">Escuela Superior de Cómputo - IPN</p>
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="https://www.escom.ipn.mx/"><i class="fas fa-university fa-2x"></i> ESCOM</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><i class="fas fa-graduation-cap fa-2x"></i></a></li>
+          
+                </ul>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Contactos <i class="fa-solid fa-user-group"></i></h5>
+                <ul>      
+                  <li>Profesora Josefina</li>
+                  <li>Porfesor Eduardo</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2022 Copyright
+            </div>
+          </div>
+        </footer>
+            
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <!--  Alert JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+    <!-- jQuery  -->
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	  <script>window.jQuery || document.write('<script src="./../../js/jquery-2.2.0.min.js"><\/script>')</script>
+    <!-- Materialize JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
+    <!-- Malihu jQuery custom content scroller JS -->
+	  <script src="./js/jquery.mCustomScrollbar.concat.min.js"></script>  
+    <!-- MaterialDark JS  -->
+	  <script src="./../../js/index.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+ 
+</body>
+</html>
